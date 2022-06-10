259c805ad6bbeb7fc5ea3a3db0f5f0d4e6dcf522
unknown <usuario@PUERTA18.CORP.AR>
2022-06-10 13:39:09 -0300

    clase del viernes 10 de junio

--- routes/router.ts
+++ routes/router.ts
@@ -6,6 +6,14 @@ export const router = Router();
 router.get('/socies', (req: Request, res: Response) => {
     res.json({
         ok: true,
-        mensaje: "funcion modificada!!"
+        mensaje: "funcion modificada!!!!!"
+    });
+});
+
+router.post('/registro' ,(req: Request, res: Response)=> {
+    const nombre = req.body.nombre;
+    res.json({
+        ok:true,
+        mensaje: `Bienvenido ${nombre}!!!`
     });
 });
